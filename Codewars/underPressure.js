2548a414d1f4b4bf7e189e12c7a0cd4ffc073a9d
Kaleigh Saphire <100825674+kaleighsaphire@users.noreply.github.com>
2022-04-07 12:15:26 -0700

    Add files via upload

---
+++ Codewars/underPressure.js
@@ -0,0 +1,13 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    i = i*2
+     return i;
+   }
+
+
+//    Alt
+//    function doubleInteger(i) {
+//     // i will be an integer. Double it and return it.
+//     return i * 2;
+//   }
