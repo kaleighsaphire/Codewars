270994c169c17346536379903bc11b413649945e
Kaleigh Saphire <100825674+kaleighsaphire@users.noreply.github.com>
2022-04-23 14:06:51 -0700

    Add files via upload

---
+++ Codewars/multiply.js
@@ -0,0 +1,12 @@
+// Implement a function which multiplies two numbers.
+const multiply = (a, b) => a * b;
+
+// Alts
+// function multiply( a, b ){
+//     return a * b;
+//   }
+
+//   function multiply(a, b){
+//     if(typeof a == 'number' && typeof b == 'number')
+//       return a * b;  
+//   }
