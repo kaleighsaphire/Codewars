4f409b462a34251c9781a05df491b087c6673bbe
Kaleigh Saphire <100825674+kaleighsaphire@users.noreply.github.com>
2022-04-26 13:53:46 -0700

    Add files via upload

---
+++ Codewars/planetID.js
@@ -0,0 +1,50 @@
+// The function is not returning the correct values. Can you figure out why?
+
+// Example (Input --> Output ):
+
+// 3 --> "Earth"
+function getPlanetName(id){
+    var name;
+    switch(id){
+      case 1:
+        name = 'Mercury'
+        break;
+      case 2:
+        name = 'Venus'
+        break;
+      case 3:
+        name = 'Earth'
+        break;
+      case 4:
+        name = 'Mars'
+        brerak;
+      case 5:
+        name = 'Jupiter'
+        break;
+      case 6:
+        name = 'Saturn'
+        break;
+      case 7:
+        name = 'Uranus'
+        break;
+      case 8:
+        name = 'Neptune'
+    }
+    
+    return name;
+  }
+  // Missing breaks. Added in.
+
+//   Alt 
+//   function getPlanetName(id){
+//     return {
+//       1: 'Mercury',
+//       2: 'Venus',
+//       3: 'Earth',
+//       4: 'Mars',
+//       5: 'Jupiter',
+//       6: 'Saturn',
+//       7: 'Uranus',
+//       8: 'Neptune'
+//     }[id]
+//   }
