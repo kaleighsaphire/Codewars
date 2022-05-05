65dd40874be7ada04f0a3b484beec062718c84b3
Kaleigh Saphire <100825674+kaleighsaphire@users.noreply.github.com>
2022-05-05 13:09:54 -0700

    Add files via upload

---
+++ Codewars/binDecimal.js
@@ -0,0 +1,9 @@
+// Complete the function which converts a binary number (given as a string) 
+// to a decimal number.
+
+function binToDec(bin){
+    let num = parseInt(bin, 2)
+    return num;
+  }
+
+  // parseInt converts string to number. Passing in 2 arguments. 
