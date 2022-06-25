3605eac5682d99aedaf2f3a83f7986c707d1d433
Kaleigh Saphire <100825674+kaleighsaphire@users.noreply.github.com>
2022-06-25 15:33:21 -0700

    string

---
+++ Codewars/combineStrings.js
@@ -0,0 +1,6 @@
+// Create a function named combineNames that accepts two parameters (first and last name). 
+// The function should return the full name.
+
+function combineNames (first, last){
+    return first + " " + last;
+}
