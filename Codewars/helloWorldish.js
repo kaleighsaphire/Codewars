568a941644b4777165a0aaff089c51940c741254
Kaleigh Saphire <100825674+kaleighsaphire@users.noreply.github.com>
2022-08-24 17:46:29 -0700

    ascii characters

---
+++ Codewars/helloWorldish.js
@@ -0,0 +1,11 @@
+// You need to create a function, helloWorld, that will return the String 
+// Hello, World! without actually using raw strings. This includes quotes, double 
+// quotes and template strings. You can, however, use the String 
+// constructor and any related functions.
+/*
+Parameters: none
+Return: "Hello, World" but without using any type of string quotes
+Example: ?
+Pseudo code: ascii numbers */
+
+const helloWorld = () => String.fromCharCode(72,101,108,108,111,44,32,87,111,114,108,100,33)
