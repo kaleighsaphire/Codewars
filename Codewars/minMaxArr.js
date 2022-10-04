c97fab9060b1fb09c72172de03a327d365589f3d
Kaleigh Saphire <100825674+kaleighsaphire@users.noreply.github.com>
2022-10-04 08:59:32 -0700

    high low

--- Codewars/minMaxArr.js
+++ Codewars/minMaxArr.js
@@ -7,7 +7,9 @@ Parameters: Given an array of integers
 Return: a sorted array of the highest value, followed by the lowest, followed by
 second hight and lowest...
 Example: ([78,44,86,2,9,23]) => [86, 2, 78, 9, 44, 23]
-Pseudo code: loop through, if e is mathmax
+Pseudo code: sort array high to low, declare new array, loop through half of array,
+push first element and last element to new array, if original array was 
+even return new array, else return new array minus last element.
 */
 
 function solve(arr){
