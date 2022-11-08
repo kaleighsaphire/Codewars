7c04db8362ce74ac0d0bf9230d89867ac1083514
Kaleigh Saphire <100825674+kaleighsaphire@users.noreply.github.com>
2022-11-08 11:58:11 -0800

    typeof

---
+++ Codewars/type.js
@@ -0,0 +1,9 @@
+/*
+Write a function that will accept two parameters: variable and type and 
+check if type of variable is matching type. Return true if types match 
+or false if not.
+*/
+
+function typeValidation(variable, type) {
+    return typeof variable == type;
+}
