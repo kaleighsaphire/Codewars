803858d078e2fae42f1da365f9c99f4d6d8f49a1
Kaleigh Saphire <100825674+kaleighsaphire@users.noreply.github.com>
2022-12-15 19:27:15 -0800

    Update abbreviate.js

--- Codewars/abbreviate.js
+++ Codewars/abbreviate.js
@@ -1,5 +1,5 @@
 // Write a function to convert a name into initials. This kata strictly takes
-//  two words with one space in between them.
+// two words with one space in between them.
 
 // The output should be two capital letters with a dot separating them.
 
