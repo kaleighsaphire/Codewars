f8c29f3d4ff1958ebc80f55a17dc5965ecdaafa3
Kaleigh Saphire <100825674+kaleighsaphire@users.noreply.github.com>
2022-12-16 20:12:59 -0800

    Update concatAB.js

--- Codewars/concatAB.js
+++ Codewars/concatAB.js
@@ -6,7 +6,7 @@ In other words, the shortest string has to be put as prefix
 and as suffix of the reverse of the longest.
 
 Strings a and b may be empty, but not null (In C# strings may 
-    also be null. Treat them as if they are empty.).
+also be null. Treat them as if they are empty.).
 If a and b have the same length treat a as the longer producing 
 b+reverse(a)+b
 
